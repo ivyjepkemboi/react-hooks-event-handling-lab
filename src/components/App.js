fd5dab12be450cf4b8f0345a8cca6cec35bc667f
Ivy <ivyjkemboi@gmail.com>
2024-05-25 10:37:57 +0300

    event

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Keypad from './Keypad';
+import EyesOnMe from './EyesOnMe';
+
+function App() {
+  return (
+    <div className="App">
+      <Keypad />
+      <EyesOnMe />
+    </div>
+  );
+}
+
+export default App;
